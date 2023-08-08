73fd72a5b15475cec1f6903c3334a596704aaf03
ahsans1997 <ahsanulhasan599@gmail.com>
2023-08-08 22:04:10 +0600

    Create ContextProvider

--- src/Components/Layouts/DefaultLayout.jsx
+++ src/Components/Layouts/DefaultLayout.jsx
@@ -1,6 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../../contexts/ContextProvider";
 
 const DefaultLayout = () => {
+    const {user, token} = useStateContext();
+
+    if (!token) {
+        return <Navigate to="/login" />
+    }
     return (
         <div>
             DefaultLayout
